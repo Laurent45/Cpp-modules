533a4872648ffff89bd872803ace054d43abc24d
Esquanor <outsider973@outlook.fr>
2022-09-24 11:38:35 +0000

    fix cpp style

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -6,13 +6,13 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 13:05:33 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/09/02 13:05:50 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/09/24 11:32:57 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <cctype>
-#include <cstdio>
 #include <iostream>
+#include <string>
+#include <locale>
 
 int main(int argc, char *argv[])
 {
@@ -20,12 +20,14 @@ int main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for (int i = 1; i < argc; i++)
+		for (int i = 1; argv[i]; i++)
 		{
-			for (int j = 0; argv[i][j]; j++)
-				std::putchar(toupper(argv[i][j]));
+			std::string str = argv[i];
+			for (size_t i = 0; i < str.length(); i++)
+				str.at(i) = std::toupper(str.at(i));
+			std::cout << str;
 		}
 		std::cout << std::endl;
 	}
-	return 0;
+	return (0);
 }
