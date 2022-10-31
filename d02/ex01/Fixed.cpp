5e23a25d40f0116ef9dd0296e1a5ad7bbf566054
Esquanor <outsider973@outlook.fr>
2022-10-31 16:29:42 +0000

    remove unused operation

--- d02/ex01/Fixed.cpp
+++ d02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/14 08:07:56 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/10/31 15:07:35 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/10/31 16:28:18 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ Fixed::Fixed(Fixed const & copy)
 }
 
 Fixed::Fixed(const int fixed_point) 
-	: _fixed_point(fixed_point * 1 << Fixed::_FRACT_BITS)
+	: _fixed_point(fixed_point << Fixed::_FRACT_BITS)
 {
 	std::cout << "Int constructor called\n";
 }
@@ -73,7 +73,7 @@ float	Fixed::toFloat(void) const
 
 int		Fixed::toInt(void) const
 {
-	return (this->_fixed_point / (1 << Fixed::_FRACT_BITS));
+	return (this->_fixed_point >> Fixed::_FRACT_BITS);
 }
 
 std::ostream	&operator<<(std::ostream &out, const Fixed &fixed) {
