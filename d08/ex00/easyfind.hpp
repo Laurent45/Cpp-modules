f88bf4c480d77003e6c5b74934b2eb025f83950a
Esquanor <outsider973@outlook.fr>
2022-11-23 17:46:13 +0000

    add const parameter

--- d08/ex00/easyfind.hpp
+++ d08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: lfrederi <lfrederi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 17:45:28 by lfrederi          #+#    #+#             */
-/*   Updated: 2022/11/23 08:58:39 by lfrederi         ###   ########.fr       */
+/*   Updated: 2022/11/23 17:45:23 by lfrederi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <algorithm>
 
 template<typename T>
-bool	easyfind(T & container, int n)
+bool	easyfind(T const & container, int n)
 {
 	if (container.end() == std::find(container.begin(), container.end(), n))
 		return (false);
